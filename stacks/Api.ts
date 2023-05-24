32896f57a8d9fa7f4a2e727d66b7e05649b943d6
jrizo0 <jr.rizo.o.jr@gmail.com>
2023-05-24 01:28:08 -0500

    api bind bucket, endpoints updates

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -1,38 +1,37 @@
 import { StackContext, Api as ApiGateway, use } from 'sst/constructs'
 import { Database } from './Database'
 import { Authentication } from './Authentication'
+import { Storage } from './Storage'
 
 export function Api({ stack }: StackContext) {
   const { table } = use(Database)
   const { auth } = use(Authentication)
+  const { bucket } = use(Storage)
 
   const api = new ApiGateway(stack, 'Api', {
     defaults: {
       function: {
-        bind: [auth, table],
+        bind: [auth, table, bucket],
       },
     },
     routes: {
-      'GET      /users/groups':               'packages/functions/src/users/listGroups.main',
-      'POST     /users/groups':               'packages/functions/src/users/addGroup.main',
-
       'POST     /groups':                     'packages/functions/src/groups/create.main',
-      'GET      /groups':                     'packages/functions/src/groups/list.main',
-      'GET      /groups/{id}':                'packages/functions/src/groups/get.main',
+      'GET      /groups/user':                'packages/functions/src/groups/list.main', // groups for user
       'PUT      /groups':                     'packages/functions/src/groups/update.main',
       'DELETE   /groups':                     'packages/functions/src/groups/delete.main',
-      'GET      /groups/users/{id}':          'packages/functions/src/groups/listUsers.main',
       'POST     /groups/add':                 'packages/functions/src/groups/addUser.main',
+      'GET      /groups/{id}/users':          'packages/functions/src/groups/listUsers.main', // users for group
 
       'POST     /bills':                      'packages/functions/src/bills/create.main',
       'GET      /bills/{id}':                 'packages/functions/src/bills/list.main',
-      'GET      /bills/{billID}/{groupID}':   'packages/functions/src/bills/get.main',
+      'GET      /bills/all':                  'packages/functions/src/bills/listAll.main',
       'PUT      /bills':                      'packages/functions/src/bills/update.main',
       'DELETE   /bills':                      'packages/functions/src/bills/delete.main',
 
-      'POST     /payments/{id}':              'packages/functions/src/payments/create.main',
+      'POST     /payments':                   'packages/functions/src/payments/create.main',
       'GET      /payments/{id}':              'packages/functions/src/payments/list.main',
-      'GET      /payments/{id}/{month}':      'packages/functions/src/payments/get.main',
+      // TODO: do i use u?
+      'GET      /payments/{bill}/{pay}':      'packages/functions/src/payments/get.main',
       // update?
       'DELETE   /payments':                   'packages/functions/src/payments/delete.main',
     },
