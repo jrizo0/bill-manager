a9eb41800e098d271eb8ed8427d1f5b938369f17
Julian Rizo <jr.rizo.o.jr@gmail.com>
2023-07-09 12:35:03 -0500

    fix: event send mail, use for instead of foreach

--- packages/functions/src/mailing/sendEventMail.ts
+++ packages/functions/src/mailing/sendEventMail.ts
@@ -5,14 +5,13 @@ import { User } from '@bill-manager/core/user'
 export async function main() {
   console.log('Creating events to send emails...')
   const unpaid = await Bill.getUnpaidOnes()
-  unpaid.data.forEach(async (bill) => {
+
+  for (const bill of unpaid.data) {
     const ownerUsers = await User.listUsersForGroup({ groupID: bill.groupID })
-    ownerUsers.data.forEach(async (user) => {
-      await Mailing.createSendMailEvent(
-        user.name,
-        user.email,
-        bill.tag,
-      )
+    console.log(ownerUsers.unprocessed)
+
+    for (const user of ownerUsers.data) {
+      await Mailing.createSendMailEvent(user.name, user.email, bill.tag)
       console.log('- Created send email event, with info')
       console.log(
         JSON.stringify({
@@ -21,6 +20,6 @@ export async function main() {
           billtag: bill.tag,
         })
       )
-    })
-  })
+    }
+  }
 }
